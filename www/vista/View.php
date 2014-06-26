bc566c36410a237bb2104dce3e160d196cf4bb8d
azrak <kas@inbox.com>
2014-06-26 16:37:22 +0200

    v

--- www/vista/View.php
+++ www/vista/View.php
@@ -130,8 +130,34 @@ class View {
         return $grid;
     }
 
-    public static function get_Category_menu($data) {
-        
+   public static function getEntity_table($entity) {
+        $data = NULL;
+        $table_head_rows = NULL;
+        $table_body_rows = NULL;
+
+        $entity_class = $entity();
+
+        $rows = $entity_class->get_list($page, $limit);
+        $prop = $entity_class->getProperty();
+        $values = $entity_class->getProperty_Values();
+
+        foreach ($prop as $th) {
+            $table_head_rows = '<th>' . $th . '</th>';
+        }
+        $data['table_head_tr'] = $table_head_rows;
+
+
+
+        foreach ($rows as $rows) {
+            $table_body_td = '';
+            foreach ($values as $value) {
+                $table_body_td.= '<td>' . $value . '</td>';
+            }
+            $table_body_rows = '<tr>' . $table_body_td . '</tr>';
+        }
+        $data['table_body_rows'] = $table_body_rows;
+
+        return $data;
     }
 
 }
